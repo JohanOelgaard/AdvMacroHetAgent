af5d71f1a4f15701a6e6aa2da3f4394808f098fe
JohanOelgaard <johan.oelgaard@gmail.com>
2023-09-28 09:59:46 +0200

    Updated folder structure

--- Assignment 1/HANCGovModel/blocks.py
+++ Assignment 1/HANCGovModel/blocks.py
@@ -1,18 +1,32 @@
 import numpy as np
 import numba as nb
 
-from GEModelTools import prev,next
+from GEModelTools import lag, lead
+
+# lags and leads of unknowns and shocks
+# K_lag = lag(ini.K,K) # copy, same as [ini.K,K[0],K[1],...,K[-2]]
+# K_lead = lead(K,ss.K) # copy, same as [K[1],K[1],...,K[-1],ss.K]
+
 
 @nb.njit
-def government(par,ini,ss,tau,G,pB,B):
+def production_firm(par,ini,ss,Gamma,K,L1,L2,rK,w1,w2,Y):
 
-    for t in range(par.T):
-        
-        B_lag = prev(B,t,ini.B)
-        tau[t] = ss.tau + par.phi*(B_lag-ss.B)
-        B[t] = (B_lag + G[t] - tau[t])/pB[t]
+    K_lag = lag(ini.K,K)
+
+    # a. implied prices (remember K and L are inputs)
+    rK[:] = par.alpha*Gamma*K_lag**(par.alpha-1.0)*L1**((1.0-par.alpha)/2.0)*L2**((1.0-par.alpha)/2.0)
+    w1[:] = Gamma*(K_lag)**par.alpha*((1.0-par.alpha)/2.0)*L1**(-(1.0+par.alpha)/2.0)*L2**((1.0-par.alpha)/2.0)
+    w2[:] = Gamma*(K_lag)**par.alpha*L1**((1.0-par.alpha)/2.0)*((1.0-par.alpha)/2.0)*L2**(-(1.0+par.alpha)/2.0)
+    
+    # b. production and investment
+    Y[:] = Gamma*K_lag**(par.alpha)*L1**((1.0-par.alpha)/2.0)*L2**((1.0-par.alpha)/2.0)
+
 
 @nb.njit
-def market_clearing(par,ini,ss,B,A_hh,clearing_B):
+def market_clearing(par,ini,ss,A,A_hh,L1,L1_hh,L2,L2_hh,Y,C_hh,K,I,clearing_A,clearing_L1,clearing_L2,clearing_Y):
 
-    clearing_B[:] = B-A_hh
+    clearing_A[:] = K-A_hh #Asset market clearing
+    clearing_L1[:] = L1-L1_hh #Labor market clearing for labor type 1
+    clearing_L2[:] = L2-L2_hh #Labor market clearing for labor type 2
+    I = K-(1-par.delta)*lag(ini.K,K) #Law of motion for capital
+    clearing_Y[:] = Y-C_hh-I #Goods market clearing
